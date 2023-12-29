eb6496a1a5b7c81aa808634c785d6556722e71a4
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2023-12-29 17:16:35 +0800

    some demonstration

---
+++ Pages/PETMEMORIAL/userprofile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pet_Reunion_Hub.Pages.PETMEMORIAL
+{
+    public class userprofileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
