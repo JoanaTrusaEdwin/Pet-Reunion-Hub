35b8902daba905b683411030e33ac3abdba762f7
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2024-03-25 01:34:28 +0800

    beautified some parts of lost pet reporting

--- Pages/LOSTPETREPORTING/NEW/Details.cshtml.cs
+++ Pages/LOSTPETREPORTING/NEW/Details.cshtml.cs
@@ -1,7 +1,51 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.EntityFrameworkCore;
+//using PRHDATALIB.Models;
+
+//namespace Pet_Reunion_Hub.Pages.LOSTPETREPORTING.NEW
+//{
+//    public class DetailsModel : PageModel
+//    {
+//        private readonly PRHDATALIB.Models.DatabaseContext _context;
+
+//        public DetailsModel(PRHDATALIB.Models.DatabaseContext context)
+//        {
+//            _context = context;
+//        }
+
+//      public CreateReport CreateReport { get; set; } = default!; 
+
+//        public async Task<IActionResult> OnGetAsync(int? id)
+//        {
+//            if (id == null || _context.CreateReport == null)
+//            {
+//                return NotFound();
+//            }
+
+//            var createreport = await _context.CreateReport.FirstOrDefaultAsync(m => m.Id == id);
+//            if (createreport == null)
+//            {
+//                return NotFound();
+//            }
+//            else 
+//            {
+//                CreateReport = createreport;
+//            }
+//            return Page();
+//        }
+//    }
+//}
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -18,25 +62,27 @@ namespace Pet_Reunion_Hub.Pages.LOSTPETREPORTING.NEW
             _context = context;
         }
 
-      public CreateReport CreateReport { get; set; } = default!; 
+        public CreateReport CreateReport { get; set; } = default!;
+        public List<IFormFile> Photos { get; set; } = new List<IFormFile>();
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null || _context.CreateReport == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var createreport = await _context.CreateReport.FirstOrDefaultAsync(m => m.Id == id);
-            if (createreport == null)
+            CreateReport = await _context.CreateReport
+                .Include(cr => cr.ReportPhotos) // Include the ReportPhotos collection
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (CreateReport == null)
             {
                 return NotFound();
             }
-            else 
-            {
-                CreateReport = createreport;
-            }
+
             return Page();
         }
     }
 }
+
