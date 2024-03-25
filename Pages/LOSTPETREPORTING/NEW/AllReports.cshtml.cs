b888f3182bf0cb3beba1d79ce0b35315815515fb
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2024-03-25 17:12:03 +0800

    a page that displays all reports by all users

---
+++ Pages/LOSTPETREPORTING/NEW/AllReports.cshtml.cs
@@ -0,0 +1,44 @@
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+
+//namespace Pet_Reunion_Hub.Pages.LOSTPETREPORTING.NEW
+//{
+//    public class AllReportsModel : PageModel
+//    {
+//        public void OnGet()
+//        {
+//        }
+//    }
+//}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PRHDATALIB.Models;
+
+namespace Pet_Reunion_Hub.Pages.LOSTPETREPORTING.NEW
+{
+    public class AllReportsModel : PageModel
+    {
+        private readonly PRHDATALIB.Models.DatabaseContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AllReportsModel(PRHDATALIB.Models.DatabaseContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IList<CreateReport> CreateReport { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            CreateReport = await _context.CreateReport.ToListAsync();
+            return Page();
+        }
+    }
+}
