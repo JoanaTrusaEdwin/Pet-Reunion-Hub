2b4422c23241407a37cd15bf58492d8835ec53a1
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2024-03-26 18:36:42 +0800

    decorated

--- Pages/PETMEMORIAL/NEW/Index.cshtml.cs
+++ Pages/PETMEMORIAL/NEW/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -9,24 +11,40 @@ using PRHDATALIB.Models;
 
 namespace Pet_Reunion_Hub.Pages.PETMEMORIAL.NEW
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly PRHDATALIB.Models.DatabaseContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IndexModel(PRHDATALIB.Models.DatabaseContext context)
+        public IndexModel(PRHDATALIB.Models.DatabaseContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<Tribute> Tribute { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
-            if (_context.Tribute != null)
+            //if (_context.Tribute != null)
+            //{
+            //    Tribute = await _context.Tribute
+            //    .Include(t => t.User).ToListAsync();
+            //}
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
             {
-                Tribute = await _context.Tribute
-                .Include(t => t.User).ToListAsync();
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
+
+            Tribute = await _context.Tribute
+                   .Where(n => n.UserId == currentUser.Id)
+                   .ToListAsync();
+
+
+            return Page();
         }
     }
 }
