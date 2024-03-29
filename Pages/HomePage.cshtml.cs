cbbcaa902a06caec87a6e0b568d1ab2db41fd63a
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2024-03-30 04:50:57 +0800

    beautified some parts, and logout button works!

--- Pages/HomePage.cshtml.cs
+++ Pages/HomePage.cshtml.cs
@@ -7,8 +7,10 @@ namespace Pet_Reunion_Hub.Pages
     [Authorize(Policy = "RequireLoggedIn")]
     public class HomePageModel : PageModel
     {
+        public bool IsUserAuthenticated { get; private set; }
         public void OnGet()
         {
+            IsUserAuthenticated = User.Identity.IsAuthenticated;
         }
     }
 }
