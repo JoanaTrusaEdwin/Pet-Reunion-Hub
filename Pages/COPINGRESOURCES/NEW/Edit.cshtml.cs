0f7c82166b2530887fc0a681c65649104c951709
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2024-03-30 21:39:09 +0800

    decorated

--- Pages/COPINGRESOURCES/NEW/Edit.cshtml.cs
+++ Pages/COPINGRESOURCES/NEW/Edit.cshtml.cs
@@ -1,77 +1,267 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.AspNetCore.Mvc.Rendering;
+//using Microsoft.EntityFrameworkCore;
+//using PRHDATALIB.Models;
+
+//namespace Pet_Reunion_Hub.Pages.COPINGRESOURCES.NEW
+//{
+//    public class EditModel : PageModel
+//    {
+//        private readonly PRHDATALIB.Models.DatabaseContext _context;
+
+//        public EditModel(PRHDATALIB.Models.DatabaseContext context)
+//        {
+//            _context = context;
+//        }
+
+//        [BindProperty]
+//        public RESOURCE RESOURCE { get; set; } = default!;
+
+//        public async Task<IActionResult> OnGetAsync(int? id)
+//        {
+//            if (id == null || _context.RESOURCE == null)
+//            {
+//                return NotFound();
+//            }
+
+//            var resource =  await _context.RESOURCE.FirstOrDefaultAsync(m => m.Id == id);
+//            if (resource == null)
+//            {
+//                return NotFound();
+//            }
+//            RESOURCE = resource;
+//           ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+//            return Page();
+//        }
+
+//        // To protect from overposting attacks, enable the specific properties you want to bind to.
+//        // For more details, see https://aka.ms/RazorPagesCRUD.
+//        public async Task<IActionResult> OnPostAsync()
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return Page();
+//            }
+
+//            _context.Attach(RESOURCE).State = EntityState.Modified;
+
+//            try
+//            {
+//                await _context.SaveChangesAsync();
+//            }
+//            catch (DbUpdateConcurrencyException)
+//            {
+//                if (!RESOURCEExists(RESOURCE.Id))
+//                {
+//                    return NotFound();
+//                }
+//                else
+//                {
+//                    throw;
+//                }
+//            }
+
+//            return RedirectToPage("./Index");
+//        }
+
+//        private bool RESOURCEExists(int id)
+//        {
+//          return (_context.RESOURCE?.Any(e => e.Id == id)).GetValueOrDefault();
+//        }
+//    }
+//}
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PRHDATALIB.Models;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Pet_Reunion_Hub.Helper;
 
 namespace Pet_Reunion_Hub.Pages.COPINGRESOURCES.NEW
 {
     public class EditModel : PageModel
     {
-        private readonly PRHDATALIB.Models.DatabaseContext _context;
+        private readonly DatabaseContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<EditModel> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public EditModel(PRHDATALIB.Models.DatabaseContext context)
+        public EditModel(ILogger<EditModel> logger, DatabaseContext context, SignInManager<IdentityUser> signInManager, IConfiguration configuration, UserManager<IdentityUser> userManager)
         {
+            _logger = logger;
             _context = context;
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [BindProperty]
-        public RESOURCE RESOURCE { get; set; } = default!;
+        public RESOURCE RESOURCE { get; set; }
+        [BindProperty]
+        public IFormFile? UploadedFile { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null || _context.RESOURCE == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var resource =  await _context.RESOURCE.FirstOrDefaultAsync(m => m.Id == id);
-            if (resource == null)
+
+            RESOURCE = await _context.RESOURCE
+                                .FirstOrDefaultAsync(m => m.Id == id);
+
+
+            if (RESOURCE == null)
             {
                 return NotFound();
             }
-            RESOURCE = resource;
-           ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+          
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
+
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Attach(RESOURCE).State = EntityState.Modified;
 
             try
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RESOURCEExists(RESOURCE.Id))
+                //if (!ModelState.IsValid)
+                //{
+                //    //If model state is invalid, return the page with validation errors
+                //    return Page();
+                //}
+
+                var user = await _userManager.GetUserAsync(User);
+                if (user != null)
                 {
-                    return NotFound();
+                    string userId = user.Id;
+                    _logger.LogInformation("User ID: {UserId}", userId);
+
+                    if (string.IsNullOrEmpty(userId))
+                    {
+                        _logger.LogWarning("User ID is null or empty.");
+                        return RedirectToPage("/Account/Login");
+                    }
+
+                    RESOURCE.UserId = userId;
+                    _logger.LogInformation("Assigned User ID: {UserId}", RESOURCE.UserId);
+                    string azureBlobStorageConnectionString = _configuration.GetConnectionString("AzureBlobStorageConnectionString");
+
+                    if (string.IsNullOrEmpty(azureBlobStorageConnectionString))
+                    {
+                        return Page();
+                    }
+
+                    if (RESOURCE.Id == 0)
+                    {
+                        if (UploadedFile != null && UploadedFile.Length > 0)
+                        {
+                            string fileUrl = await UploadPhotoToBlobStorage(UploadedFile);
+                            RESOURCE.FILEURL = fileUrl;
+                        }
+                        //_context.CreateReport.Add(CreateReport);
+                        RESOURCE toBeUpdated = await _context.RESOURCE.FindAsync(RESOURCE.Id);
+                        if (toBeUpdated != null)
+                        {
+                            //Console.WriteLine($"!!!!!!!!!!!!!!!!!!!!tobeupdated: ID: {toBeUpdated.Id}");
+                            //await TryUpdateModelAsync<Tribute>(toBeUpdated, "Tribute", c => c.PetName, c => c.DateOfBirth, );
+                        }
+                        _context.RESOURCE.Update(RESOURCE);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        var existingRESOURCE = await _context.RESOURCE.FirstOrDefaultAsync(m => m.Id == RESOURCE.Id);
+                        if (existingRESOURCE == null)
+                        {
+                            return NotFound();
+                        }
+
+                        existingRESOURCE.TITLE = RESOURCE.TITLE;
+                        existingRESOURCE.DESCRIPTION = RESOURCE.DESCRIPTION;
+                        existingRESOURCE.LINK = RESOURCE.LINK;
+                        existingRESOURCE.FILEURL = RESOURCE.FILEURL;
+                        existingRESOURCE.TYPE = RESOURCE.TYPE;
+                        existingRESOURCE.FORMAT = RESOURCE.FORMAT;
+
+
+                        if (UploadedFile != null && UploadedFile.Length > 0)
+                        {
+                            string fileUrl = await UploadPhotoToBlobStorage(UploadedFile);
+                            existingRESOURCE.FILEURL = fileUrl;
+                        }
+
+                        //existingTribute.PetName = EncryptionHelper.Encrypt(existingTribute.PetName);
+                        //existingTribute.PetType = EncryptionHelper.Encrypt(existingTribute.PetType);
+                        //existingTribute.PetBreed = EncryptionHelper.Encrypt(existingTribute.PetBreed);
+                        //existingTribute.PetSex = EncryptionHelper.Encrypt(existingTribute.PetSex);
+                        //existingTribute.Cause = EncryptionHelper.Encrypt(existingTribute.Cause);
+                        //existingTribute.TributeText = EncryptionHelper.Encrypt(existingTribute.TributeText);
+                        //existingTribute.Visibility = EncryptionHelper.Encrypt(existingTribute.Visibility);
+
+                        _context.Entry(existingRESOURCE).State = EntityState.Modified;
+
+                    }
+
+                    await _context.SaveChangesAsync();
+                    _logger.LogInformation("Tribute saved successfully.");
+
+                    return RedirectToPage("./Index");
                 }
                 else
                 {
-                    throw;
+                    _logger.LogError("User not found.");
+                    return RedirectToPage("/Account/Login");
                 }
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error saving report:");
 
-            return RedirectToPage("./Index");
+                throw;
+            }
+
         }
 
-        private bool RESOURCEExists(int id)
+        private async Task<string> UploadPhotoToBlobStorage(IFormFile UploadedFile)
         {
-          return (_context.RESOURCE?.Any(e => e.Id == id)).GetValueOrDefault();
+            string azureBlobStorageConnectionString = _configuration.GetConnectionString("AzureBlobStorageConnectionString");
+            if (string.IsNullOrEmpty(azureBlobStorageConnectionString))
+            {
+                return null;
+            }
+
+            string containerName = "tributes";
+
+            var blobServiceClient = new BlobServiceClient(azureBlobStorageConnectionString);
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(UploadedFile.FileName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            using (var stream = UploadedFile.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, true);
+            }
+
+            return blobClient.Uri.ToString();
         }
     }
 }
--- Pages/COPINGRESOURCES/NEW/Index.cshtml.cs
+++ Pages/COPINGRESOURCES/NEW/Index.cshtml.cs
@@ -1,32 +1,110 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.EntityFrameworkCore;
+//using PRHDATALIB.Models;
+
+//namespace Pet_Reunion_Hub.Pages.COPINGRESOURCES.NEW
+//{
+//    public class IndexModel : PageModel
+//    {
+//        private readonly PRHDATALIB.Models.DatabaseContext _context;
+
+//        public IndexModel(PRHDATALIB.Models.DatabaseContext context)
+//        {
+//            _context = context;
+//        }
+
+//        public IList<RESOURCE> RESOURCE { get;set; } = default!;
+
+//        public async Task OnGetAsync()
+//        {
+//            if (_context.RESOURCE != null)
+//            {
+//                RESOURCE = await _context.RESOURCE
+//                .Include(r => r.User).ToListAsync();
+//            }
+//        }
+//    }
+//}
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Pet_Reunion_Hub.Helper;
 using PRHDATALIB.Models;
 
 namespace Pet_Reunion_Hub.Pages.COPINGRESOURCES.NEW
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly PRHDATALIB.Models.DatabaseContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IndexModel(PRHDATALIB.Models.DatabaseContext context)
+        public IndexModel(PRHDATALIB.Models.DatabaseContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IList<RESOURCE> RESOURCE { get;set; } = default!;
+        public IList<RESOURCE> RESOURCE { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
-            if (_context.RESOURCE != null)
+            //if (_context.Tribute != null)
+            //{
+            //    Tribute = await _context.Tribute
+            //    .Include(t => t.User).ToListAsync();
+            //}
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
             {
-                RESOURCE = await _context.RESOURCE
-                .Include(r => r.User).ToListAsync();
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
+
+            RESOURCE = await _context.RESOURCE
+                   .Where(n => n.UserId == currentUser.Id)
+                   .ToListAsync();
+
+            //foreach (var tribute in Tribute)
+            //{
+            //    // Check if the user ID associated with the tribute matches the currently authenticated user
+            //    if (tribute.UserId == currentUser.Id)
+            //    {
+            //        // Decrypt the tribute properties
+            //        tribute.PetName = EncryptionHelper.Decrypt(tribute.PetName);
+            //        tribute.PetType = EncryptionHelper.Decrypt(tribute.PetType);
+            //        tribute.PetBreed = EncryptionHelper.Decrypt(tribute.PetBreed);
+            //        tribute.PetSex = EncryptionHelper.Decrypt(tribute.PetSex);
+            //        tribute.Cause = EncryptionHelper.Decrypt(tribute.Cause);
+            //        tribute.TributeText = EncryptionHelper.Decrypt(tribute.TributeText);
+            //        //tribute.TributePhoto = EncryptionHelper.Decrypt(tribute.TributePhoto);
+            //        tribute.Visibility = EncryptionHelper.Decrypt(tribute.Visibility);
+            //        //tribute. = EncryptionHelper.Decrypt(tribute.);
+            //        //tribute. = EncryptionHelper.Decrypt(tribute.);
+            //        // Decrypt other properties as needed
+            //    }
+            //    else
+            //    {
+            //        // If the user ID associated with the tribute doesn't match the currently authenticated user, skip decryption
+            //        // Alternatively, you can choose to handle this case differently based on your application's requirements
+            //    }
+            //}
+
+
+            return Page();
         }
     }
 }
+
