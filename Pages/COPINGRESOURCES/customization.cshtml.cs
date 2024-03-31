f2a6b873e3d5af2aa67900786569c6def6eefb05
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2024-04-01 06:33:53 +0800

    can display resources

--- Pages/COPINGRESOURCES/customization.cshtml.cs
+++ Pages/COPINGRESOURCES/customization.cshtml.cs
@@ -1,12 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Pet_Reunion_Hub.Helper;
+using PRHDATALIB.Models;
 
 namespace Pet_Reunion_Hub.Pages.COPINGRESOURCES
 {
+    [Authorize]
     public class customizationModel : PageModel
     {
-        public void OnGet()
+        private readonly DatabaseContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public customizationModel(DatabaseContext context, UserManager<IdentityUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public List<RESOURCE> RESOURCE { get; set; }
+
+
+        public IActionResult OnGet()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            // Query for public tributes
+            RESOURCE = _context.RESOURCE
+                .Where(t => t.TYPE == "Customisation")
+                .OrderByDescending(t => t.Id)
+                .ToList();
+
+            Console.WriteLine($"{RESOURCE.Count}");
+
+            return Page();
         }
     }
 }
+
