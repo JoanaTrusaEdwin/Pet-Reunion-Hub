17cd34bc966a4a4b26780cc071ed5a969dd41a39
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2024-04-24 22:27:28 +0800

    does its job but looks messy

--- Pages/HomePage.cshtml.cs
+++ Pages/HomePage.cshtml.cs
@@ -1,16 +1,57 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PRHDATALIB.Models;
 
 namespace Pet_Reunion_Hub.Pages
 {
-    [Authorize(Policy = "RequireLoggedIn")]
+    //[Authorize(Policy = "RequireLoggedIn")]
+    [Authorize]
     public class HomePageModel : PageModel
     {
-        public bool IsUserAuthenticated { get; private set; }
-        public void OnGet()
+        //public bool IsUserAuthenticated { get; private set; }
+        //public void OnGet()
+        //{
+        //    IsUserAuthenticated = User.Identity.IsAuthenticated;
+        //}
+
+        private readonly PRHDATALIB.Models.DatabaseContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomePageModel(PRHDATALIB.Models.DatabaseContext context, UserManager<IdentityUser> userManager)
         {
-            IsUserAuthenticated = User.Identity.IsAuthenticated;
+            _context = context;
+            _userManager = userManager;
         }
+       
+
+        public IList<CreateReport>? CreateReport { get; set; }
+        public GENERALLOCATION? GENERALLOCATION { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            // Get the current user
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                // Handle case where user is not found
+                return;
+            }
+
+            // Retrieve the LOCATIONVALUE of the current user
+            var userLocationValue = await _context.GENERALLOCATION
+                .Where(gl => gl.UserId == currentUser.Id)
+                .Select(gl => gl.LOCATIONVALUE)
+                .FirstOrDefaultAsync();
+
+            // Query reports where GenLoc matches the user's LOCATIONVALUE
+            CreateReport = await _context.CreateReport.Include(r => r.ReportPhotos).Where(r => r.GenLoc == userLocationValue).ToListAsync();
+        }
+
     }
 }
