f1193d074e7fc27114efd5dd06e12cb0c65822f0
JoanaTrusaEdwin <bcs21090005@student.uts.edu.my>
2024-05-15 02:01:11 +0800

    REF POINT 4: REMOVED PHONE NUMBER SAVING IN MANAGE ACC (_MANAGENAV.CSHTML AND MANAGEMAVPAGES.CS)

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -17,7 +17,7 @@ namespace  Pet_Reunion_Hub.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public static string Index => "Index";
+        //public static string Index => "Index";
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -65,7 +65,7 @@ namespace  Pet_Reunion_Hub.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+        //public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
